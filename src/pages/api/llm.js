bd07a24b485f30318fa07a682e54db9570d72bb4
Ashish <ashish25895@gmail.com>
2023-05-26 17:30:47 +0530

    Added API and page

---
+++ src/pages/api/llm.js
@@ -0,0 +1,22 @@
+import { createLLMService } from "usellm";
+
+const llmService = createLLMService({
+  openaiApiKey: process.env.OPEN_API_KEY,
+  actions: ["chat"],
+});
+
+export const runtime = "edge";
+
+export default async function POST(request) {
+  const body = await request.json();
+
+  try {
+    const { result } = await llmService.handle({ body, request });
+    return new Response(result, { status: 200 });
+  } catch (error) {
+    console.error("[LLM Service]", error);
+    return new Response(error.message, {
+      status: error?.status || 400,
+    });
+  }
+}
